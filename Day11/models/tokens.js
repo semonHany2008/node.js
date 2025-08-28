b9c3cfcfbe3db59e66bd3b20aea894881fa03af8
semonHany2008 <semonhany848@gmail.com>
2025-08-28 10:33:49 +0300

    Delete Day11 directory

--- Day11/models/tokens.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const tokensSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    createdAt: { type: Date, default: Date.now, expires: 60 },
-  },
-});
-const tokensModel = mongoose.model("tokens", tokensSchema);
-module.exports = { tokensModel };
